8ab1e8b64c09b61e243362dc027d1746a118f808
jamesbonzani <57076724+jamesbonzani@users.noreply.github.com>
2022-05-04 10:30:20 -0400

    dev

--- src/main/java/com/kingcast255/hextechmod/common/item/HextechGauntletItem.java
+++ src/main/java/com/kingcast255/hextechmod/common/item/HextechGauntletItem.java
@@ -1,29 +1,17 @@
 package com.kingcast255.hextechmod.common.item;
 
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
-import net.minecraft.world.level.ClipContext;
-import net.minecraft.world.level.CollisionGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.shapes.EntityCollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
 
 
 public class HextechGauntletItem extends Item {
@@ -126,7 +114,7 @@ public class HextechGauntletItem extends Item {
 			Vec3 vec = player.getLookAngle().normalize();
 			player.push(vec.x*f, vec.y*f, vec.z*f);
 			CompoundTag tag = new CompoundTag();
-			stack.getTag().putBoolean("hextechmod.player_pushed",true);
+			
 
 			removeFullyCharged(stack);
 			
@@ -138,31 +126,8 @@ public class HextechGauntletItem extends Item {
 		
 	}
 
-	/*
-	@Override
-	public void inventoryTick(ItemStack stack, Level level, Entity ent, int x, boolean b) {
-		if (!level.isClientSide && ent instanceof Player) {
-			if (stack.hasTag() && stack.getTag().get("hextechmod.player_pushed") != null) {
-				if (stack.getTag().getBoolean("hextechmod.player_pushed")) {
-					if (((Player) ent).isOnGround()) {
-						stack.getTag().putBoolean("hextechmod.player_pushed", false);
-					} else {
-						java.util.List<VoxelShape> collisions = level.getEntityCollisions(
-								ent, new AABB(ent.getX() - 10D, ent.getY() - 10D, ent.getZ() - 10D,
-										ent.getX() + 10D, ent.getY() + 10D, ent.getZ() + 10D)
-						);
-						for (VoxelShape vshape : collisions) {
-							if (!vshape.isEmpty()) {
-								((Player) ent).sendMessage(new TextComponent("Collided with Entity"), ent.getUUID());
-							}
-						}
-
-					}
-				}
-			}
-		}
-	}
-*/
+	
+	
 	public UseAnim getUseAnimation(ItemStack p_40678_) {
 		      return UseAnim.BOW;
 		   }
